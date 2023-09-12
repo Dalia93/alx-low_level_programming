9529d26ec814c9c0018789b9f21220f226cad645
Dalia@93 <daliabderheem93@gmail.com>
2023-09-12 14:02:35 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - this is the a fuction
  * 0c: check input of function
- *i
+ *
  * Return: return 1 if 'c' is lowercase
  * otherwise always 0 (success)
  */
@@ -13,7 +13,6 @@ int _islower(int c)
 {
 if (c >= 97 && c <= 122)
 return (1);
-
 return (0);
 
 }
