050a90fb954b9fa98eeae2a4d2c8b33a67e853e5
Dalia@93 <daliabderheem93@gmail.com>
2023-10-20 09:29:35 -0700

    Singly linked lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,6 +13,7 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *new;
 list_t *temp = *head;
+ 
 unsigned int len = 0;
 
 while (str[len])
@@ -31,9 +32,12 @@ if (*head == NULL)
 *head = new;
 return (new);
 }
+
 while (temp->next)
 temp = temp->next;
+
 temp->next = new;
+
 return (new);
 }
 
